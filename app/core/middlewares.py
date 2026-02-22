3d1f7a41731b10768cdeb2d2a37d9b2cd2cdfd35
Ghislain Z. <gzeleu@outlook.de>
2026-02-22 17:53:09 +0100

    Response headers Option

---
+++ app/core/middlewares.py
@@ -0,0 +1,42 @@
+# https://fastapi.tiangolo.com/tutorial/middleware/
+# - Create a Middleware
+# - Add a middleware to app
+#   - Two ways, decorator ==> @app.middleware("http")
+#   - Add app.addleware_func()
+
+
+import time
+
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from fastapi import Request, Response
+
+
+# Use-Cases:
+# - Logging (Requests / Responses)
+# - Auth / API-Key prüfen
+# - Request-Zeit messen
+# - Header hinzufügen --> Done
+# - Exception-Handling
+
+
+class CustomMiddleware(BaseHTTPMiddleware):
+    """We call this: class based Middleware ..."""
+
+    async def __add_process_time_header(self, request: Request, call_next) -> None:
+        """Header hinzufügen"""
+        start_time = time.perf_counter()  # why perf_counter here?
+        response: Response = await call_next(request)
+        process_time = 1000 * (time.perf_counter() - start_time)
+        #  TODO: Understand Response headers Option see in api-docs
+        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers
+        response.headers[
+            "X-Process-Time-Ghislain-this-is-how-to-add-extra-info-in-header-custom-middleware-ghislain"
+        ] = f"{str(process_time)} ms"
+        for item_tuple in response.headers.items():
+            print(item_tuple)
+        return response
+
+    async def dispatch(self, request: Request, call_next) -> Response:
+        respo = await self.__add_process_time_header(request, call_next)
+        return respo
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from app.db import create_db_and_tables, seed_db_if_empty
 from app.routers.auth_routes import router as auth_router
 from app.routers.order_routes import router as order_router
 from app.routers.customer_routes import router as customer_router
+from app.core.middlewares import CustomMiddleware
 
 
 @asynccontextmanager
@@ -14,10 +17,17 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, title="PizzaDeliveryApp")
 
+# Add Middlewares
+app.add_middleware(CustomMiddleware)
 
 # Include router from different API
 app.include_router(auth_router)
 app.include_router(order_router)
 app.include_router(customer_router)
+
+
+@app.get("/")
+def root():
+    return {"Hello welcome to my first FastAPI real world Project called Delivery API"}
